c959bcc596439da668200fef8bdd29cec1c37955
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2026-05-31 10:02:49 +0200

    Initial commit for the option code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ulikunitz/opt
+
+go 1.26.3
---
+++ option.go
@@ -0,0 +1,37 @@
+package opt
+
+import "encoding/json"
+
+type Value[T any] struct {
+	V  T
+	Ok bool
+}
+
+func Val[T any](v T) Value[T] { return Value[T]{V: v, Ok: true} }
+
+func NoVal[T any]() Value[T] {
+	var zero T
+	return Value[T]{V: zero, Ok: false}
+}
+
+func (o *Value[T]) MarshalJSON() ([]byte, error) {
+	if o.Ok {
+		return json.Marshal(o.V)
+	}
+	return []byte("null"), nil
+}
+
+func (o *Value[T]) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		o.Ok = false
+		var zero T
+		o.V = zero
+		return nil
+	}
+	err := json.Unmarshal(data, &o.V)
+	if err != nil {
+		return err
+	}
+	o.Ok = true
+	return nil
+}
