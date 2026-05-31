b6524792154de4adf409ef62faa3839f25ee4bf2
Ulrich Kunitz <ulikunitz@users.noreply.github.com>
2026-05-31 10:51:05 +0200

    option.go: added documentation comments

--- option.go
+++ option.go
@@ -5,18 +5,24 @@ package opt
 
 import "encoding/json"
 
+// Value represents an optional Value. The value V can only be interpreted if Ok
+// is true. Note that the zero value is not a valid entry.
 type Value[T any] struct {
 	V  T
 	Ok bool
 }
 
+// Returns a valid Value.
 func Val[T any](v T) Value[T] { return Value[T]{V: v, Ok: true} }
 
+// NoVal returns an invalid Value.
 func NoVal[T any]() Value[T] {
 	var zero T
 	return Value[T]{V: zero, Ok: false}
 }
 
+// MarshalJSON implements the json.Marshaler interface. If the Value is valid,
+// it marshals the value V. If the Value is invalid, it marshals to null.
 func (o Value[T]) MarshalJSON() ([]byte, error) {
 	if o.Ok {
 		return json.Marshal(o.V)
@@ -24,6 +30,9 @@ func (o Value[T]) MarshalJSON() ([]byte, error) {
 	return []byte("null"), nil
 }
 
+// UnmarshalJSON implements the json.Unmarshaler interface. If the input is null,
+// it sets the Value to invalid. Otherwise, it tries to unmarshal the input into
+// the value V and sets the Value to valid if successful.
 func (o *Value[T]) UnmarshalJSON(data []byte) error {
 	if string(data) == "null" {
 		o.Ok = false
